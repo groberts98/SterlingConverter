0d871c56ac80cfdec06a82a67e3b5e6dd40fc84e
George Roberts <george.t.roberts98@gmail.com>
2021-08-19 12:18:57 +0100

    Implement function to convert input to pence

--- SterlingConverter.java
+++ SterlingConverter.java
@@ -9,7 +9,7 @@ public class SterlingConverter {
         try {
             while(true) {
                 input = sc.nextLine();
-                output = sterlingConverter.convert(Integer.parseInt(input));
+                output = sterlingConverter.convert(sterlingConverter.formatInput(input));
                 System.out.println(output);
             }
         } finally {
@@ -24,7 +24,38 @@ public class SterlingConverter {
 
     //function to format the user input
     public int formatInput(String input) {               
-        return 0;
+        int value = 0;
+        input = input.charAt(input.length()-1) == 'p' ? input.substring(0, input.length()-1) : input;
+        boolean containsPound = input.charAt(0) == '\u00A3';
+        if(input.contains(".")) {
+            input = containsPound ? input.substring(1) : input;
+            String[] parts = input.split("\\.");
+            int pounds = 0, pennies = 0;
+            if (parts.length > 0) {
+                //if pounds section exists and is non empty, extract the value
+                pounds = parts[0] == "" ? 0 : Integer.parseInt(parts[0]);
+            }
+            if(parts.length == 2) {
+                if(parts[1].length() == 1) {
+                    //if the pence section has only 1 digit, multiply it by 10
+                    pennies = 10*Integer.parseInt(parts[1]);
+                } else if(parts[1].length() > 2) {
+                    pennies = Integer.parseInt(parts[1].substring(0, 2));
+                    if(Character.getNumericValue(parts[1].charAt(2)) > 4) {
+                        //if the pence section is longer than two digits and the third digit is greater than 4, round up.
+                        pennies ++;
+                    }
+                } else {
+                    pennies = Integer.parseInt(parts[1]);
+                }
+            }
+            value = 100*pounds + pennies;
+        } else {
+            //if the input has no point and has a pound symbol, multiply value by 100
+            value = containsPound ? 100*Integer.parseInt(input.substring(1)) : Integer.parseInt(input);
+        }
+
+        return value;
     }
 
     //function to convert pennies to sterling coins
@@ -44,10 +75,8 @@ public class SterlingConverter {
                     result.append(coinAmount + " x " + coinValues[i] + "p, ");
                 }
             }
-
             total = total % coinValues[i];
         }
-
         String output = result.toString();
         output = output == "" ? output : output.substring(0, output.length()-2); //remove the final ", " at the end of the string
 
